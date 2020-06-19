50d8e69f558f294b8957a6a3ede13135277d6180
napawanboon <bnapawan4nf@gmail.com>
2020-06-19 21:57:44 +0700

    Change Haha tag a in oage index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,6 @@
 @section('page_title', 'Test Hello')
 @section('content')
 <p>Hello World {{ $name }} {{ $lastname }} </p>
-    <a href="{{ url('/test') }}">Go To Test page</a>
+    <a href="{{ url('/test') }}">Go To Test page HAHA</a>
     <img src="{{ url('images/1.png') }}" alt="Girl in a jacket" width="500" height="600">
 @stop 
